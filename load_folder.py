50a7015b549b9921909a1b460582c50999c90b85
vanessamillang <144068457+vanessamillang@users.noreply.github.com>
2023-09-25 23:31:55 +0000

    nuevo avance

--- load_folder.py
+++ load_folder.py
@@ -1,10 +1,10 @@
-import os
 import redis
+import os
 import re
-# https://github.com/redis/redis-py
+from bs4 import BeautifulSoup
+import itertools
 
 r = redis.Redis(host='localhost', port=6379, db=0)
-
 
 def load_folder(path):
     files = os.listdir(path)
@@ -14,8 +14,26 @@ def load_folder(path):
         if match:
             with open(path + file) as f:
                 html = f.read()
+                book_id = match.group(1)
+
                 r.set(match.group(1), html)
+                create_index(book_id, html)
             print(match.group(0), match.group(1))
 
+def create_index(book_id, html):
+    soup = BeautifulSoup(html, 'html.parser')
+    texto = soup.get_text()
+    
+    palabras = re.findall(r'\w+', texto, re.IGNORECASE)
+    
+    for palabra in palabras:
+        palabra_lower = palabra.lower()
+        r.sadd(palabra, book_id)  # Original 
+        r.sadd(palabra_lower, book_id)  # Minúsculas
+        r.sadd(palabra.upper(), book_id)  # Mayúsculas
+        
+        combinaciones = [''.join(p) for p in itertools.product(*zip(palabra_lower, palabra_lower.upper()))]
+        for combinacion in combinaciones:
+            r.sadd(combinacion, book_id)
 
 load_folder('html/books/')
--- webserver.py
+++ webserver.py
@@ -2,12 +2,14 @@ from functools import cached_property
 from http.cookies import SimpleCookie
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import parse_qsl, urlparse
+import redis
 
-# Código basado en:
-# https://realpython.com/python-http-server/
-# https://docs.python.org/3/library/http.server.html
-# https://docs.python.org/3/library/http.cookies.html
-
+# Configuración
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
+SERVER_HOST = '0.0.0.0'
+SERVER_PORT = 8000
 
 class WebRequestHandler(BaseHTTPRequestHandler):
     @cached_property
@@ -18,36 +20,40 @@ class WebRequestHandler(BaseHTTPRequestHandler):
     def query_data(self):
         return dict(parse_qsl(self.url.query))
 
-    @cached_property
-    def post_data(self):
-        content_length = int(self.headers.get("Content-Length", 0))
-        return self.rfile.read(content_length)
-
-    @cached_property
-    def form_data(self):
-        return dict(parse_qsl(self.post_data.decode("utf-8")))
-
-    @cached_property
-    def cookies(self):
-        return SimpleCookie(self.headers.get("Cookie"))
-
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
         self.end_headers()
-        self.wfile.write(self.get_response().encode("utf-8"))
+        books = None
+        if self.query_data and 'q' in self.query_data:
+            books = self.search_books(self.query_data['q'].split(' '))
+        self.wfile.write(self.get_response(books).encode("utf-8"))
 
-    def get_response(self):
-        return f"""
-    <h1> Hola Web </h1>
-    <p>  {self.path}         </p>
-    <p>  {self.headers}      </p>
-    <p>  {self.cookies}      </p>
-    <p>  {self.query_data}   </p>
-"""
+    def search_books(self, keywords):
+        r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+        return r.sinter(keywords)
 
+    def get_response(self, books):
+        response_html = """
+        <h1> Libros de misterio </h1>
+        <form action="/" method="get">
+              <label for="q"> Busqueda </label>
+              <input type="text" name="q" required/>
+              <input type="submit" value="Buscar"/>
+        </form>
+
+        <p> Resultado de la busqueda: {} </p>
+        """.format(books if books else "No se encontraron resultados")
+
+        return response_html
 
 if __name__ == "__main__":
-    print("Server starting...")
-    server = HTTPServer(("0.0.0.0", 8000), WebRequestHandler)
-    server.serve_forever()
+    try:
+        r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+        r.ping()  # Comprobar la conexión a Redis
+    except redis.ConnectionError:
+        print("Error: No se puede conectar a Redis. Asegúrate de que esté en ejecución en {}:{}".format(REDIS_HOST, REDIS_PORT))
+    else:
+        print(f"Server starting on {SERVER_HOST}:{SERVER_PORT}...")
+        server = HTTPServer((SERVER_HOST, SERVER_PORT), WebRequestHandler)
+        server.serve_forever()
